e3ff37519a13cf067ef34fa724cf509df0dabddc
Paulo Cezar Lima Zampieri <pczampieri@hotmail.com>
2019-08-14 15:52:37 -0300

    firt commit

---
+++ src/routes.js
@@ -0,0 +1,27 @@
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+
+import Main from './screens/Main';
+import User from './screens/User';
+import Repository from './screens/Repository';
+
+const Routes = createAppContainer(
+  createStackNavigator(
+    {
+      Main,
+      User,
+      Repository,
+    },
+    {
+      headerLayoutPreset: 'center',
+      headerBackTitleVisible: false,
+      defaultNavigationOptions: {
+        headerStyle: {
+          backgroundColor: '#7159c1',
+        },
+        headerTintColor: '#fff',
+      },
+    }
+  )
+);
+
+export default Routes;
